f16cb71b4aeb85c5e1f6c3643a1652a93f611ce1
nescafe3n1 <leueilshem@gmail.com>
2026-03-11 13:22:51 +0800

    lab-act-instance

---
+++ naranjo-client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Welcome to my React App!</h1>
+      <p>
+        Name: Leueil Shem Naranjo <br />
+        Email: leueilshem@gmail.com <br />
+        Other Personal Info: Pinoy
+      </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
